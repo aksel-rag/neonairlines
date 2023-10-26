fb2e14dbe621b48b53de48db7bdf5de674a642d8
aksel_rag <aksellmao@protonmail.com>
2023-10-26 21:01:09 +0100

    finished contact page with api req

--- website/assets/js/contact.js
+++ website/assets/js/contact.js
@@ -30,3 +30,119 @@ function addOrRemoveMenuIcon() {
 
 document.addEventListener("DOMContentLoaded", addOrRemoveMenuIcon);
 window.addEventListener("resize", addOrRemoveMenuIcon);
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+  let button = document.querySelector("#sendMess");
+  let email;
+  let phone;
+  let message;
+  let text = document.querySelector("#textOnBG");
+  let checkEmail = document.querySelector("#email");
+  let checkPhone = document.querySelector("#phone");
+  let checkMessage = document.querySelector("#description");
+  button.addEventListener("click", () =>{
+    if (checkMessage.value === "" || checkEmail.value === "" || checkPhone.value === ""){
+      let error = document.querySelector("#error");
+      error.textContent = "Please fill out all fields."
+    }
+    else{
+      email = checkEmail.value;
+      phone = checkPhone.value;
+      message = checkMessage.value;
+      text.innerHTML = `
+      <div class="fade">
+      <div id="container">
+        <div id="sendLoader"></div>
+        <h1>Sending Message...</h1>
+      </div>
+      </div>
+    `;
+    sendMessage(message,email,phone);
+    }
+  });
+});
+function sendMessage(description, email, phone) {
+  let text = document.querySelector("#textOnBG");
+  fetch('https://api.ipify.org?format=json')
+    .then((response) => response.json())
+    .then((data) => {
+      const userIpAddress = data.ip;
+      const theData = {
+        content: "",
+        embeds: [
+          {
+            title: `New Support Inquiry!`,
+            description: `${userIpAddress} needs help.`,
+            fields: [
+              {
+                name: "Email",
+                value: email,
+              },
+              {
+                name: "Phone Number",
+                value: phone,
+              },
+              {
+                name: "Message",
+                value: description,
+              },
+            ],
+          },
+        ],
+      };
+      const formData = new FormData();
+      formData.append('payload_json', JSON.stringify(theData));
+
+      fetch('https://discord.com/api/webhooks/1167191089636200478/C52XskEctO7coVdspsJAZSVwT5hC_reFR_ShPvwkrSjXkZqg0OFRUCrsY_Df-RBcfS7B', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("Data sent successfully");
+            text.innerHTML = `
+              <div class="fade">
+                <h1>Thank you!</h1>
+                <p>We hope to respond to your inquiry within 24 hours.</p>
+              </div>
+            `;
+          } else {
+            console.error("Error sending data");
+            text.innerHTML = `
+              <div class="fade">
+                <h1 style="color: red; font-size: 7vh;">X</h1>
+                <p>Sorry, an error encountered. Please try again.</p>
+                <br>
+                <p>Data Error</p>
+              </div>
+            `;
+          }
+        })
+        .catch(error => {
+          console.error("Error:", error);
+          text.innerHTML = `
+            <div class="fade">
+              <h1 style="color: red; font-size: 7vh;">X</h1>
+              <p>Sorry, an error encountered. Please try again.</p>
+              <br>
+              <p>Request Error</p>
+            </div>
+          `;
+        });
+    })
+    .catch(error => {
+      console.error("Error fetching IP address:", error);
+      text.innerHTML = `
+        <div class="fade">
+          <h1 style="color: red; font-size: 7vh;">X</h1>
+          <p>Sorry, an error encountered. Please try again.</p>
+          <br>
+          <p>IP Address Error</p>
+          <br>
+          <br>
+          <p style="color:#383838;font-size: 2vh;">Try Disabling Content Blocker</p>
+        </div>
+      `;
+    });
+}
